5161bd1131705cd0f7b4fedf2ac37448dbe6a122
Tomasz Szewczyk <tomasz.szewczyk02@gmail.com>
2017-11-08 17:41:14 +0100

    2.1

---
+++ 2.1/tests/tests.py
@@ -0,0 +1,16 @@
+import unittest
+import subprocess
+
+class ExeTest(unittest.TestCase):
+	def execute(self, path, stdin, stdout):
+		proc = subprocess.Popen(
+			path,
+			stdout=subprocess.PIPE,
+		    stdin=subprocess.PIPE)
+		proc.stdin.write(bytes(stdin, 'ascii'))
+		proc.stdin.close()
+		result = proc.stdout.read()
+		result = result.decode()
+		result = result.strip(' \n')
+		assert result == stdout, bytes(result, 'ascii')
+		proc.wait()
