7a00247319b677e153875aeda518cdf649d22226
Tomasz Szewczyk <tomasz.szewczyk02@gmail.com>
2018-01-09 01:21:01 +0100

    added 2.018

--- 2.018/source/main.cpp
+++ 2.018/source/main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 
-#include "main.h"
-
 using namespace std;
+
+/*
+ * Napisać program wyliczający pierwiastek równania x^x=2017 metodą stycznych.
+ */
+
+int f(int x) { return x*x - 2017; }
+int f_prim(int x) { return 2*x; }
 
 #ifndef TEST
 int main()
 {
-  int a, b;
-  cin >> a >> b;
+	const int N = 100000000;
+	const int a = 0;
+	double x = f(a);
+	for(int i = 0; i < N; i++)
+		x -= (double) f(x) / (double) f_prim(x);
 
-  int c = add(a, b);
-
-  cout << c;
+	cout << x << endl;
 
   return 0;
 }
 #endif
-
-int add(int a, int b)
-{
-  return a + b;
-}
